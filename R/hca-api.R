e768b7a7ab77d9315e2c08b966cfdf4ea28b08ca
Daniel Van Twisk <daniel.vantwisk@roswellpark.org>
2018-08-02 15:23:11 -0400

    Added method definitions for hca dcp-api implementation

--- R/hca-api.R
+++ R/hca-api.R
@@ -17,7 +17,7 @@
     response <- httr::POST(url, headers, body = body)
     stop_for_status(response)
     response
-}
+}   b
 
 bundles_checkout <-
     function(uuid)
@@ -26,3 +26,77 @@ bundles_checkout <-
     response <- .hcaget(url)
     ## ...
 }
+
+.getBundleCheckout <-
+    function(replica=c('aws', 'gcp', 'azure'), checkout_job_id)
+{
+
+}
+
+.getBundle <-
+    function(uuid, replica=c('aws', 'gcp', 'azure'), version=NULL,
+        directurls=TRUE, presignedurls=TRUE, token=NULL)
+{
+
+}
+
+.putBundle <-
+    function(uuid, replica=c('aws', 'gcp', 'azure'), request=NULL, version=NULL)
+{
+
+}
+
+.putCollections <-
+    function(uuid, replica=c('aws', 'gcp', 'azure'), request=NULL, version=NULL)
+{
+
+}
+
+.getCollections <-
+    function(uuid, replica=c('aws', 'gcp', 'azure'), version=NULL)
+{
+
+}
+
+.getFiles <-
+    function(uuid, replica=c('aws', 'gcp', 'azure'), token=NULL, version=NULL)
+{
+
+}
+
+.putFiles <-
+    function(uuid, request=NULL, version=NULL)
+{
+
+}
+
+.getSubscriptions <-
+    function(replica=c('aws', 'gcp', 'azure'))
+{
+
+}
+
+.putSubscriptions <-
+    function(replica=c('aws', 'gcp', 'azure'), request=NULL)
+{
+
+}
+
+.getSubscription <-
+    function(uuid, replica=c('aws', 'gcp', 'azure'))
+{
+
+}
+
+#' 
+setMethod("getBundlesCheckout", "character", .getBundlesCheckout)
+setMethod("getBundle", "character", .getBundle)
+setMethod("putBundle", "character", .putBundle)
+setMethod("putCollections", "character", .putCollections)
+setMethod("getCollections", "character", .getCollections)
+setMethod("getFiles", "character", .getFiles)
+setMethod("putFiles", "character", .putFiles)
+setMethod("getSubscriptions", "character", .getSubscriptions)
+setMethod("putSubscriptions", "character", .putSubscriptions)
+setMethod("getSubscriptions", "character", .getSubscriptions)
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(HCABrowser)
+
+test_check("HCABrowser")
