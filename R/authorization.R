099ceb851f11c72a361b27a32b8b6b0233f00579
Daniel Van Twisk <daniel.vantwisk@roswellpark.org>
2018-08-14 04:46:51 -0400

    Changed to token authorization, documentation and small fixes to previous code.

--- R/authorization.R
+++ R/authorization.R
@@ -1,19 +1,30 @@
 #' @importFrom httr oauth_app oauth_endpoints oauth2.0_token
+#' @importFrom googleAuthR gar_auth gar_auto_auth gar_set_client
 #' @export
 
 get_token <- function() {
-  if(Sys.getenv('HCA_TOKEN') != '')  return(Sys.getenv('GDC_TOKEN'))
-  token_file <- "~/.hca_token"
-  if(file.exists(token_file))
-    return(suppressWarnings(readLines(token_file,n=1)))
+#  if(Sys.getenv('HCA_TOKEN') != '')  return(Sys.getenv('GDC_TOKEN'))
+#  token_file <- "~/.hca_token"
+#  if(file.exists(token_file))
+#    return(suppressWarnings(readLines(token_file,n=1)))
 
-  oauth_endpoints("google")
+#  oauth_endpoints("google")
 
-  myapp <- oauth_app("google",
-    key = "16795585089.apps.googleusercontent.com",
-    secret = "fj4D0Dj1o9KCx1478IiiLMnM")
+#  myapp <- oauth_app("google",
+#    key = "16795585089.apps.googleusercontent.com",
+#    secret = "fj4D0Dj1o9KCx1478IiiLMnM")
 
-  google_token <- oauth2.0_token(oauth_endpoints("google"), myapp,
-    scope = "https://www.googleapis.com/auth/userinfo.email",
-    cache = token_file)
+#  google_token <- oauth2.0_token(oauth_endpoints("google"), myapp,
+#    scope = "https://www.googleapis.com/auth/userinfo.email",
+#    cache = token_file)
+
+  required_scopes <- c("https://www.googleapis.com/auth/userinfo.email")
+  
+  gar_set_client(system.file("client.json", package = "HCABrowser"),
+                 scopes = required_scopes)
+  
+  gar_auto_auth(required_scopes,
+                new_user = FALSE,
+                no_auto = FALSE,
+                environment_var = "BQ_AUTH_FILE")
 }
--- R/hca-api.R
+++ R/hca-api.R
@@ -24,7 +24,7 @@
     if(!is.null(uuid)) tag <- sprintf(tag, uuid)
     url <- paste0(url, tag)
     if(!is.null(args)) {
-        args[vapply(a, is.null, logical(1))] <- NULL
+        args[vapply(args, is.null, logical(1))] <- NULL
         args <- paste0(names(args), rep("=", length(args)), as.character(args))
         args <- paste0(args, collapse="&")
         url <- paste0(url, "?", args)
@@ -32,10 +32,21 @@
     url
 }
 
+#' @importFrom httr add_headers
+.build_header <- function()
+{
+    token <- get_token()
+    add_headers(
+        access_token = token$credentials[['access_token']]#,
+#        token_type = token$credentials[['token_type']],
+#        expires_in = token$credentials[['expires_in']]
+    )
+}
+
 #' @importFrom httr content stop_for_status
 #' @importFrom jsonlite fromJSON
 .return_reponse <-
-    function(reponse)
+    function(response)
 {
     stop_for_status(response)
     response <- content(response, as = "text")
@@ -46,7 +57,8 @@
 .hca_delete <-
     function(url, body)
 {
-    reponse <- httr:DELETE(url, body, encode="json")
+    header <- .build_header()
+    response <- httr::DELETE(url, header, body=body, encode="json")
     .return_reponse(response)
 } 
 
@@ -54,7 +66,8 @@
 .hca_get <-
     function(url)
 {
-    response <- httr::GET(url)
+    header <- .build_header()
+    response <- httr::GET(url, header)
     .return_reponse(response)
 }
 
@@ -62,7 +75,17 @@
 .hca_head <-
     function(url)
 {
-    response <- httr::HEAD(url)
+    header <- .build_header()
+    response <- httr::HEAD(url, header)
+    .return_reponse(response)
+}
+
+#' @importFrom httr PATCH
+.hca_post <-
+    function(url, body)
+{
+    header <- .build_header()
+    response <- httr::PATCH(url, header, body=body, encode="json")
     .return_reponse(response)
 }
 
@@ -70,7 +93,8 @@
 .hca_post <-
     function(url, body)
 {
-    response <- httr::POST(url, body, encode="json")
+    header <- .build_header()
+    response <- httr::POST(url, header, body=body, encode="json")
     .return_reponse(response)
 }
 
@@ -78,7 +102,8 @@
 .hca_put <-
     function(url, body)
 {
-    reponse <- httr::PUT(url, body, encode="json")
+    header <- .build_header()
+    reponse <- httr::PUT(url, header, body, encode="json")
     .return_reponse(response)
 }
 
@@ -97,7 +122,7 @@
         reason = NULL, url='https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica, version=version)
+    args <- list(replica=replica, version=version)
     url <- .build_url(url, .apis['deleteBundle'], uuid, args)
     .hca_delete(url)
 }
@@ -108,7 +133,7 @@
         url='https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica, version=version, directurls=directurls,
+    args <- list(replica=replica, version=version, directurls=directurls,
                   presignedurls=presignedurls, token=token)
     url <- .build_url(url, .apis['getBundle'], uuid, args)
     .hca_get(url)
@@ -119,7 +144,7 @@
         creator_uid, files, url='https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica, version=version)
+    args <- list(replica=replica, version=version)
     body <- list(creator_uid=creator_uid, files=files)
     url <- .build_url(url, .apis['putBundle'], uuid, args)
     .hca_put(url, body)
@@ -136,7 +161,7 @@
         email=NULL, url='https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica, version=version)
+    args <- list(replica=replica, version=version)
     body <- list(destination=destination, email=email)
     url <- .build_url(url, .apis['postBundlesCheckout'], uuid, args)
     .hca_post(url, body)
@@ -169,18 +194,18 @@
         url='https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica, version=version)
+    args <- list(replica=replica, version=version)
     url <- .build_url(url, .apis['getCollection'], uuid, args)
     .hca_get(url)
 }
 
 .patchCollection <-
     function(uuid, replica=c('aws', 'gcp', 'azure'), version, add_contents,
-        description, details, name, remove-contents,
+        description, details, name, remove_contents,
         url = 'https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica, version=version)
+    args <- list(replica=replica, version=version)
     body <- list(add_contents=add_contents, description=description,
                  details=details, name=name, remove_contents=remove_contents)
     url <- .build_url(url, .apis['patchCollection'], uuid, args)
@@ -192,7 +217,7 @@
         url = 'https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica, version=version, token=token)
+    args <- list(replica=replica, version=version, token=token)
     url <- .build_url(url, .apis['getFile'], uuid, args)
     .hca_get(url)
 }
@@ -202,7 +227,7 @@
         url = 'https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica, version=version)
+    args <- list(replica=replica, version=version)
     url <- .build_url(url, .apis['getFile'], uuid, args)
     .hca_head(url)
 }
@@ -212,10 +237,24 @@
         url = 'https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(version=version)
+    args <- list(version=version)
     body <- list(creator_uid=creator_uid, source_url=source_url)
     url <- .build_url(url, .apis['putFile'], uuid, args)
     .hca_put(url, body)
+}
+
+.postSearch <-
+    function(replica=c('aws', 'gcp', 'azure'),
+        output_format=c('summary', 'raw'), es_query, per_page=100,
+        search_after=NULL, url = 'https://dss.data.humancellatlas.org/v1')
+{
+    replica <- match.arg(replica)
+    output_format <- match.arg(output_format)
+    args <- list(replica=replica, output_format=output_format,
+                 per_page=per_page, search_after=search_after)
+    body <- list(es_query=es_query)
+    url <- .build_url(url, .apis['postSearch'], NULL, args)
+    .hca_post(url, body)
 }
 
 .getSubscriptions <-
@@ -223,8 +262,8 @@
         url = 'https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica)
-    url <- .build_url(url, .apis['getSubscriptions'] args)
+    args <- list(replica=replica)
+    url <- .build_url(url, .apis['getSubscriptions'], NULL, args)
     .hca_get(url)
 }
 
@@ -235,7 +274,7 @@
         url = 'https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica)
+    args <- list(replica=replica)
     body <- list(attachments=attachments, callback_url=callback_url,
                  encoding=encoding, es_query=es_query, form_fields=form_fields,
                  hmac_key_id=hmac_key_id, hmac_secret_key=hmac_secret_key,
@@ -249,7 +288,7 @@
         url = 'https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica)
+    args <- list(replica=replica)
     url <- .build_url(url, .apis['deleteSubscription'], uuid, args)
     .hca_delete(url)
 }
@@ -259,14 +298,178 @@
         url = 'https://dss.data.humancellatlas.org/v1')
 {
     replica <- match.arg(replica)
-    args <- list=(replica=replica)
+    args <- list(replica=replica)
     url <- .build_url(url, .apis['getSubscription'], uuid, args)
     .hca_get(url)
 }
 
+#' @name HCA API methods
+#'
+#' @aliases getBundlesCheckout deleteBundle getBundle putBundle
+#'      postBundlesCheckout putCollection deleteCollection getCollection
+#'      patchCollection getFile headFile putFile postSearch getSubscriptions
+#'      putSubscription deleteSubscription getSubscription
+#'
+#' @description
+#'
+#' Methods to access the Human Cell Atlas's Data Coordination Platform (HCA DCP)
+#' by means of the platform's REST API.
+#'
+#' @usage
+#'
+#' getBundlesCheckout(checkout_job_id, ...)
+#' deleteBundle(uuid, ...)
+#' getBundle(uuid, ...)
+#' putBundle(uuid, ...)
+#' postBundle(uuid, ...)
+#' postBundlesCheckout(uuid, ...)
+#' putCollection(uuid, ...)
+#' deleteCollection(uuid, ...)
+#' getCollection(uuid, ...)
+#' patchCollection(uuid, ...)
+#' getFile(uuid, ...)
+#' headFile(uuid, ...)
+#' putFile(uuid, ...)
+#' postSearch(replica, ...)
+#' getSubscriptions(replica, ...)
+#' putSubscription(replica, ...)
+#' deleteSubscription(replica, ...)
+#' getSubscription(replica, ...)
+#' 
+#' @param add_contents list. List of items to remove from the collection. Items
+#'  must match exactly to be removed. Items not found in the collection are
+#'  ignored. (ADD DESCRIPTION OF LIST OBJECT)
+#'
+#' @param attachments list. The set of bundle metadata items to be included in
+#'  the payload of a notification request to a subscriptionendpoint. Each
+#'  property in this object represents an attachment to the notification
+#'  payload. Each attachment will be a child property of the "attachments"
+#'  property of the payload. The name of such a child property can be chosen
+#'  freely provided it does not start with an underscore. For example, if the
+#'  subscription is ``` { "attachments": { "taxon": { "type": "jmespath",
+#'  "expression": "files.biomaterial_j
+#'  son.biomaterials[].content.biomaterial_core.ncbi_taxon_id[]" } } } ``` the
+#'  corresponding notification payload will contain the following entry ```
+#'  "attachments": { "taxon": [9606, 9606] } ``` If a general error occurs
+#'  during the processing of attachments, the notification will be sent with
+#'  `attachments` containing only the reserved `_errors` attachment containing a
+#'  string describing the error. If an error occurs during the processing of a
+#'  specific attachment, the notification will be sent with all
+#'  successfully processed attachments and additionally
+#'  the `_errors` attachment containing an object with one
+#'  property for each failed attachment. For example, ```
+#'  "attachments": { "taxon": [9606, 9606] "_errors" {
+#'  "biomaterial": "Some error occurred" } } ``` The value
+#'  of the `attachments` property must be less than or
+#'  equal to 128 KiB in size when serialized to JSON and
+#'  encoded as UTF-8. If it is not, the notification will
+#'  be sent with "attachments": { "_errors": "Attachments
+#'  too large (131073 bytes)" }
+#'
+#' @param callback_url character(1).
+#'  The subscriber's URL. An HTTP request is made to the
+#'  specified URL for every attempt to deliver a
+#'  notification to the subscriber. If the HTTP response
+#'  code is 2XX, the delivery attempt is considered
+#'  successful and no more attemtpts will be made.
+#'  Otherwise, more attempts will be made with an
+#'  exponentially increasing delay between attempts, until
+#'  an attempt is successful or the a maximum number of
+#'  attempts is reached.
+#'
+#' @param checkout_job_id character(1). A RFC4122-complliant ID for the checkout
+#'  job request.
+#'
+#' @param contents list. A list of objects describing links to files, bundles,
+#'  other collections, and metadata fragments that are part of the collection.
+#'
+#' @param creator_uid character(1). User ID who is creating this bundle.
+#'
+#' @param description character(1). A long description of the collection,
+#'  formatted in Markdown.
+#'
+#' @param destination character(1). User-owned destination storage bucket.
+#'
+#' @param details list. Supplementary JSON metadata for the collection.
+#'  (ADD DESCRIPTION OF STRUCTURE)
+#'
+#' @param directurls logical(1). Include direct-access URLs in the response.
+#'  This is mutually exclusive with the \code{presignedurls} parameter.  DEFAULT
+#'  is \code{NULL}.
+#'
+#' @param email character(1). An email address to send status updates to.
+#'
+#' @param encoding character(1). The MIME type describing the encoding of the
+#'  request body.  Either "application/json" or "multipart/form-data".
+#'
+#' @param es_query list. Elasticsearch query. (ADD DESCRIPTION OF STRUCTURE)
+#'
+#' @param files list. (ADD DESCRIPTION ON STRUCTURE OF THIS ARGUMENT)
+#'
+#' @param form_fields list. A collection of static form fields to be supplied in
+#'  the request body, alongside the actual notification payload.
+#'
+#' @param hmac-key-id character(1). An optional key ID to use with
+#'  "hmac_secret_key".
+#'
+#' @param hmac_secret_key character(1). The key for signing requests to the
+#'  subscriber's URL. The signature will be constructed according to
+#'  https://tools.ietf.org/html/draft-cavage-http-signatures and transmitted in
+#'  the HTTP `Authorization` header.
+#'
+#' @param method The HTTP request method to use when delivering a notification
+#'  to the subscriber.
+#'
+#' @param name character(1). A short name identifying the collection.
+#'
+#' @param output_format character(1). Specifies the output format. Either
+#'  "summary" or "raw". The default format, "summary", is a list of UUIDs for
+#'  bundles that match the query. Set this parameter to "raw" to get the
+#'  verbatim JSON metadata for bundles that match the query.
+#'
+#' @param payload_form_field character(1). The name of the form field that will
+#'  hold the notification payload when the request is made. If the default name
+#'  of the payload field collides with that of a field in `form_fields`, this
+#'  porperty can be used to rename the payload and avoid the collision. This
+#'  property is ignored unless `encoding` is `multipart/form-data`.
+#'
+#' @param per_page numeric(1). Max number of results to return per page.
+#'
+#' @param presignedurls logical(1). Include presigned URLs in the response. This
+#'   is mutually exclusive with the directurls parameter.
+#'
+#' @param reason character(1). User-friendly reason for the bundle or timestamp-
+#'   specific bundle deletion.
+#'
+#' @param remove-contents list. List of items to remove from the collection.
+#'  Items must match exactly to be removed. Items not found in the collection
+#'  are ignored.
+#'
+#' @param replica character(1). A replica to fetch form. Can either be
+#'  set to "aws", "gcp", or "azure".  DEFAULT is "aws".
+#'
+#' @param search_after character(1). **Search-After-Context**. An internal state
+#'  pointer parameter for use with pagination. The API client should not need to
+#'  set this parameter directly; it should instead directly fetch the URL given
+#'  in the "Link" header.
+#'
+#' @param source_url character(1). Cloud URL for source data.
+#'
+#' @param token \code{Token}. Token to manage retries. End users constructing
+#'   queries should not set this parameter. Use \code{get_token()} to generate.
+#'
+#' @param uuid character(1). A RFC4122-compliant ID for the bundle.
+#'
+#' @param version character(1). Timestamp of bundle creation in RFC3339.
+#'
+#' 
+#'
 #' Check the status of a checkout request 
 #'
 #' @description Check the status of a checkout request 
+#'
+#' @author Daniel Van Twisk
+#'
 #' @export
 setMethod("getBundlesCheckout", "character", .getBundlesCheckout)
 
@@ -289,7 +492,7 @@ setMethod("putBundle", "character", .putBundle)
 #' destination
 #'
 #' @export
-setMethod("postBundlesCheckout", "character", .postBundleCheckout)
+setMethod("postBundlesCheckout", "character", .postBundlesCheckout)
 
 #' Create a collection
 #'
