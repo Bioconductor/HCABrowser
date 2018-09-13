c0fe92f0b0d16c7f19f7ea03f6ea02b1d4d2e67f
Daniel Van Twisk <daniel.vantwisk@roswellpark.org>
2018-09-13 16:01:06 -0400

    Functioning iteration over SearchResult using .parse_postSearch_results

--- R/iteration-utilities.R
+++ R/iteration-utilities.R
@@ -11,7 +11,7 @@
 )
 
 .ignore_fields <- c(
-    'file_json',
+#    'file_json',
     'links_json'
 )
 
@@ -19,24 +19,32 @@
     'describedBy'
 )
 
+#' @importFrom plyr rbind.fill
 #' @importFrom stringr str_sub
-.dataframe_from_postSearch_results <- function(results)
+.parse_postSearch_results <- function(results)
 {
     results <- results(results)
 
-    ## aquire bundle ids
-    bundle_fqids <- vapply(results, function(i) {
-        i[["bundle_fqid"]]
-    }, character(1))
-
-    ## aquire bundles urls
-    bundle_urls <- vapply(results, function(i) {
-        i[["bundle_url"]]
-    }, character(1))
-
     bundle_files <- lapply(results, function(i) {
         a <- do.call(rbind.data.frame, i[['metadata']][['manifest']][['files']])
-        a[!grepl('.json$', a$name),]
+        a <- a[!grepl('.json$', a$name),]
+        a[order(a['name']),]
+    })
+
+    ## aquire bundle ids
+    bundle_fqids <- lapply(seq_along(results), function(i) {
+        a <- rep(results[[i]][["bundle_fqid"]], length(bundle_files[[i]][['name']]))
+        a <- as.data.frame(a)
+        names(a) <- 'bundle_fqid'
+        a
+    })
+
+    ## aquire bundles urls
+    bundle_urls <- lapply(seq_along(results), function(i) {
+        a <- rep(results[[i]][["bundle_url"]], length(bundle_files[[i]][['name']]))
+        a <- as.data.frame(a)
+        names(a) <- 'bundle_url'
+        a
     })
 
     ## acquire rest of json schema information for bundles
@@ -44,7 +52,7 @@
         field_names <- names(results[[i]][["metadata"]][["files"]])
         field_names <- field_names[!field_names %in% .ignore_fields]
         dfs <- lapply(field_names, function(x) {
-            .obtain_content(results[[i]], x, length(bundle_files[[i]]))
+            .obtain_content(results[[i]], x, length(bundle_files[[i]][['name']]))
         })
         names(dfs) <- field_names
         dfs
@@ -52,42 +60,46 @@
 
     browser()
 
-    lapply(bundle_files, function(jsons) {
-        lapply(jsons, function(i) {
-            
-        })
+    all_files <- lapply(seq_along(bundle_files), function(i) {
+        do.call(cbind.data.frame, c(list(bundle_files[[i]], bundle_fqids[[i]],
+            bundle_urls[[i]]), json_bundles[[i]]))
     })
+    
+    all_files <- do.call(plyr::rbind.fill, all_files)
 
-    ## combine evertyhing to one table
-#    json_bundles['bundle_fqids'] <- rep(bundle_fqids, each=permutation_val)
-#    json_bundles['bundle_urls'] <- rep(bundle_urls, each=permutation_val)
-
-    json_bundles
+    all_files
 }
 
 .obtain_content <- function(results, field_name, n)
 {
     sub <- .field_sub_directory[[field_name]]
-#    if (field_name %in% 'file_json')
-#        do.call(rbind.data.frame, content_dir)
-    if (field_name %in% c('project_json')) {
+    if (field_name %in% 'file_json') {
+        content_dir <- results[["metadata"]][["files"]][[field_name]][[sub]]
+        content <- lapply(seq_along(content_dir), function(i) {
+            data.frame(as.list(unlist(content_dir[[i]][['content']])))
+        })
+        x <- do.call(plyr::rbind.fill, content)
+        x <- x[!duplicated(x),]
+        x[order(x['file_core.file_name']),]
+    }
+    else if (field_name %in% c('project_json')) {
         x <- results[["metadata"]][["files"]][[field_name]]
         x <- unlist(x)
         nam <- paste0(basename(x['describedBy']), '.', names(x))
         names(x) <- nam
         x <- as.data.frame(as.list(x))
         x <- x[rep(seq_len(nrow(x)), n), ]
+        x
     }
     else {
         content_dir <- results[["metadata"]][["files"]][[field_name]][[sub]]
-        lapply(content_dir, function(x) {
+        res <- lapply(content_dir, function(x) {
             x <- unlist(x)
             nam <- paste0(basename(x['content.describedBy']), '.', names(x))
             names(x) <- nam
-            x <- as.data.frame(as.list(x))
-            x <- x[rep(seq_len(nrow(x)), n), ]
-            browser()
-            do.call(cbind, x)
+            x
         })
+        res <- as.data.frame(as.list(unlist(res)))
+        res[rep(seq_len(nrow(res)), n), ]
     }
 }
